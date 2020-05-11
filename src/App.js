efdb0f4eb9d83210b8e61fc06156175182e3a920
shanudjn <shanudjn@gmail.com>
2020-05-11 12:39:41 +0530

    Added remaining code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import ListItems from './ListItems';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faTrash);
 
 class App extends Component {
   constructor(props){
@@ -13,6 +18,9 @@ class App extends Component {
     }
     this.handleInput = this.handleInput.bind(this);
     this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate= this.setUpdate.bind(this);
+
   }
 
   handleInput(e){
@@ -21,13 +29,49 @@ class App extends Component {
        text : e.target.value,
        key : Date.now()
      }
-    })
+    })  
   }
 
   addItem(e){
     e.preventDefault();
-    const newItem = this.currentItem;
+    const newItem = this.state.currentItem;
     console.log(newItem);
+    if(newItem.text!==""){
+      const newItems = [...this.state.items,newItem]
+      this.setState({
+        items : newItems,
+        currentItem:{
+          text : '',
+          key : ''          
+        }
+      })
+    }
+    console.log(this.state.items);
+  }
+
+  deleteItem(key){
+    const filteredItem = this.state.items.filter(item => 
+      item.key!==key);
+      this.setState({
+        items : filteredItem,
+        currentItem :{
+          text:'',
+          key:''
+        }
+      })
+  }
+
+  setUpdate(text, key){
+    const items = this.state.items;
+    items.map(item=>{      
+      if(item.key===key){
+        console.log(item.key +"    "+key)
+        item.text= text;
+      }
+    })
+    this.setState({
+      items : items
+    })
   }
   render() {
     return (
@@ -36,7 +80,9 @@ class App extends Component {
           <input type="text" placeholder="Enter Task" value={this.state.currentItem.text} onChange={this.handleInput}></input>
           <button type="submit">Add</button>
         </form>
+        <ListItems items={this.state.items} deleteItem={this.deleteItem} setUpdate={this.setUpdate}/>
       </div>
+      
     );
   }
 }
